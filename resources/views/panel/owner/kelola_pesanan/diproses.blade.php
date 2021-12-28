9ec6f526e44fffb3e8d1b1a0e524a472ce3be331
rahmat-ilyas <rahmat.ilyas142@gmail.com>
2021-12-28 12:23:18 +0800

    Bismillah

--- resources/views/panel/owner/kelola_pesanan/diproses.blade.php
+++ resources/views/panel/owner/kelola_pesanan/diproses.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.layouts')
 
 @section('content')
-    <div class="content-wrapper">
-        <div class="row">
-            <div class="col-md-12 grid-margin">
-                <div class="row">
-                    <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                        <!-- <h3 class="font-weight-bold">Selamat Datang {{ Auth::user()->name }} </h3> -->
-                        <h6 class="font-weight-normal mb-0">Ini Adalah Panel Administrator <span
-                                class="text-primary">Tomoniwalet.com</span></h6>
+@php
+$pesanan = new App\transaksi;
+$pesanan = $pesanan->where('status', '!=', 'selesai')->get();
+@endphp
+<div class="content-wrapper">
+    <div class="row">
+        <div class="col-md-12 grid-margin">
+            <div class="row">
+                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                    <!-- <h3 class="font-weight-bold">Selamat Datang {{ Auth::user()->name }} </h3> -->
+                    <h6 class="font-weight-normal mb-0">Ini Adalah Panel Administrator <span
+                        class="text-primary">Tomoniwalet.com</span></h6>
                     </div>
 
                 </div>
@@ -21,7 +25,7 @@
                     <div class="card-body">
                         <h4 class="card-title">Tabel Produk</h4>
 
-                        <table class="table dataTable">
+                        <table class="table" id="dataTable">
                             <thead>
                                 <tr>
                                     <th>Kode</th>
@@ -31,11 +35,23 @@
                                     <th>Telepon</th>
                                     <th>Jasa Kirim</th>
                                     <th>Ongkir</th>
+                                    <th>Status</th>
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
-
+                                @foreach ($pesanan as $dta)
+                                <tr>
+                                    <td>{{ $dta->kode }}</td>
+                                    <td>{{ date('d/m/Y', strtotime($dta->tanggal)) }}</td>
+                                    <td>{{ $dta->user->name }}</td>
+                                    <td>{{ $dta->alamat }}</td>
+                                    <td>{{ $dta->telepon }}</td>
+                                    <td>{{ strtoupper($dta->jasa_kirim) }}</td>
+                                    <td>Rp{{ number_format($dta->biaya_ongkir) }}</td>
+                                    <td>{{ strtoupper($dta->status) }}</td>
+                                </tr>
+                                @endforeach
                             </tbody>
                         </table>
 
@@ -45,49 +61,75 @@
         </div>
 
     </div>
-@endsection
+    @endsection
 
-@push('scripts')
+    @push('scripts')
     <script>
         $(function() {
-            var dataTable = $('.dataTable').DataTable();
+            var dataTable = $('#dataTable').DataTable();
             dataTable.clear().draw();
-            dataTable.row.add([
-                'no',
-                'vl.kd_pemesanan',
-                'vl.nama_pemesan',
-                'vl.nama_pemesan',
-                'vl.nama_alat',
-                'vl.jumlah_hilang',
-                'vl.tanggal_hilang',
-                '<td class="text-center"><a href="#" class="btn btn-sm btn-default" id="set-alat-kembali" data-id="1" ><i class="fa fa-reply"></i> Telah Kembali</a> </td>'
-            ]).draw(false);
+            // dataTable.row.add([
+            //     'no',
+            //     'vl.kd_pemesanan',
+            //     'vl.nama_pemesan',
+            //     'vl.nama_pemesan',
+            //     'vl.nama_alat',
+            //     'vl.jumlah_hilang',
+            //     'vl.tanggal_hilang',
+            //     '<td class="text-center"><a href="#" class="btn btn-sm btn-default" id="set-alat-kembali" data-id="1" ><i class="fa fa-reply"></i> Telah Kembali</a> </td>'
+            // ]).draw(false);
 
-            // $.ajax({
-            //     url: host + "/api/inventori/getalathilang",
-            //     method: "GET",
-            //     headers: headers,
-            //     success: function(data) {
-            //         var tableAlatHilang = $('#tableAlatHilang').DataTable();
-            //         tableAlatHilang.clear().draw();
-            //         var no = 1;
-            //         $.each(data.result, function(key, vl) {
-            //             tableAlatHilang.row.add([
-            //                 no,
-            //                 vl.kd_pemesanan,
-            //                 vl.nama_pemesan,
-            //                 vl.nama_alat,
-            //                 vl.jumlah_hilang,
-            //                 vl.tanggal_hilang,
-            //                 `<td class="text-center">
-        //                         <a href="#" class="btn btn-sm btn-default" id="set-alat-kembali" data-id="` + vl
-            //                 .id + `" ><i class="fa fa-reply"></i> Telah Kembali</a>
-        //                         </td>`
-            //             ]).draw(false);
-            //             no = no + 1;
-            //         });
-            //     }
-            // });
+
+            tb_supplier = $("#dataTable").DataTable({
+                bLengthChange: true,
+                bFilter: true,
+                bInfo: true,
+                bAutoWidth: true,
+                searching: true,
+                processing: true,
+                serverSide: true,
+                
+                deferRender: true,
+                responsive: !0,
+                colReorder: !0,
+                pagingType: "full_numbers",
+                stateSave: !1,
+                language: {
+                    zeroRecords: "Belum ada data...",
+                    processing: '<span class="text-danger">Mengambil Data....</span>',
+                },
+                columns: [
+                { data: "id" },
+                { data: "kode" },
+                { data: "nama" },
+                { data: "telepon" },
+                { data: null },
+                ],
+                columnDefs: [
+                {
+                    defaultContent: "-",
+                    targets: "_all",
+                },
+                {
+                    targets: 0,
+                    className: "dt-left",
+                    orderable: false,
+                    searchable: false,
+                    visible: false,
+                },
+                {
+                    targets: -1,
+                    title: "Actions",
+                    orderable: false,
+                    searchable: false,
+                    width: "250px",
+                    sClass: "text-center",
+                    render: function (data) {
+                        return `<button class="btn btn-warning mr-2" onclick="overlayForm('owner/supplier/edit/${data.id}','Update Data Supplier')" ><i class="fa fa-edit"></i> Edit</button><button class="btn btn-danger mr-2" onclick="delete_data('owner/supplier/destroy/${data.id}','tb_supplier')"><i class="fa fa-trash"></i> Hapus</button>`;
+                    },
+                },
+                ],
+            });
         });
     </script>
-@endpush
+    @endpush
